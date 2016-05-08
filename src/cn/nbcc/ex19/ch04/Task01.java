756280af66f0f530ee91d94d1442f8e1e3fedaad
nbcc-java <18842426@qq.com>
2016-05-08 12:42:24 +0800

    add ex19 task01

---
+++ src/cn/nbcc/ex19/ch04/Task01.java
@@ -0,0 +1,50 @@
+package cn.nbcc.ex19.ch04;
+
+
+public class Task01 {
+
+	static class Person{
+		String name;
+		Person(String name){
+			this.name = name;
+		}
+		public void speak(){}
+		public void eat(){}
+	}
+	static class American extends Person{
+		public American(String name) {
+			super(name);
+		}
+		@Override
+		public void speak() {
+			System.out.println(name+"speak american english!");
+		}
+		@Override
+		public void eat() {
+			System.out.println(name+"eat food using forks and knivies");
+		}
+	}
+	static class Chinese extends Person{
+		public Chinese(String name){
+			super(name);
+		}
+		@Override
+		public void speak() {
+			System.out.println(name+"说汉语!");
+		}
+		@Override
+		public void eat() {
+			System.out.println(name+"用筷子吃东西");
+		}
+	}
+	
+	public static void main(String[] args) {
+		American a = new American("Michael");
+		Chinese c = new Chinese("张三");
+		a.speak();
+		a.eat();
+		c.speak();
+		c.eat();
+	}
+
+}
