75974a9cdf0640b80e99751719a88a6f70f8e2d9
nbcc-java <18842426@qq.com>
2016-05-08 16:35:48 +0800

    commit ex19 Task02

--- src/cn/nbcc/ex19/ch04/Task02.java
+++ src/cn/nbcc/ex19/ch04/Task02.java
@@ -1,11 +1,58 @@
 package cn.nbcc.ex19.ch04;
 
 public class Task02 {
+	
+	static class Point{
+		int x,y;
+		Point(int x,int y){
+			this.x = x;
+			this.y = y;
+		}
+		@Override
+		public String toString() {
+			return String.format("[x=%d,y=%d]", x,y);
+		}
+	}
+	static class Circle extends Point{
+		private int r;
+
+		Circle(int x,int y,int r){
+			super(x,y);
+			this.r = r;
+		}
+		public double getPerimeter() {
+			return Math.PI*2*r;
+		}
+		public double getArea(){
+			return Math.PI*r*r;
+		}
+		@Override
+		public String toString() {
+			return String.format("%s,r=%d,area=%.2f,peri=%.2f", super.toString(),r,getArea(),getPerimeter());
+		}
+	}
+	static class Cylinder extends Circle{
+		int height;
+		Cylinder(Circle circle,int height){
+			super(circle.x,circle.y,circle.r);
+			this.height=height;
+		}
+		public double getVolumn() {
+			return getArea()*height;
+		}
+		@Override
+		public String toString() {
+			return String.format("%s h=%d vol=%.2f", super.toString(),height,getVolumn());
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("add task 02");
-
+		Point a = new Point(10, 20);
+		Circle b = new Circle(0, 0, 1);
+		Cylinder c = new Cylinder(new Circle(0,0,1), 10);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
 	}
 
 }
