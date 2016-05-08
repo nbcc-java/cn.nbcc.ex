ab8a7750460e66e3d53bf6a1deee8b2f58fd6d7b
nbcc-java <18842426@qq.com>
2016-05-08 13:13:44 +0800

    add ex19 task02 blank file

---
+++ src/cn/nbcc/ex19/ch04/Task02.java
@@ -0,0 +1,11 @@
+package cn.nbcc.ex19.ch04;
+
+public class Task02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("add task 02");
+
+	}
+
+}
