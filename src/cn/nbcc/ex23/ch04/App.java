b0a0c50f42e2abe57b796fcb536aff065ea1fb3e
nbcc-java <18842426@qq.com>
2016-06-15 22:29:53 +0800

    add ex23

--- src/cn/nbcc/ex23/ch04/App.java
+++ src/cn/nbcc/ex23/ch04/App.java
@@ -3,29 +3,33 @@ package cn.nbcc.ex23.ch04;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 public class App {
-	
-	static class Student{
+	static class Student {
 		String name;
-		public Student(String name){
-			this.name=name;
+
+		public Student(String name) {
+			this.name = name;
 		}
+
 		public String getName() {
 			return name;
 		}
+
 		@Override
 		public boolean equals(Object obj) {
-			if (obj==this) {
+			if (obj == this) {
 				return true;
 			}
-			if (obj.getClass()!=this.getClass()) {
+			if (obj.getClass() != this.getClass()) {
 				return false;
 			}
-			Student other = (Student)obj;
-			
+			Student other = (Student) obj;
+
 			return this.getName().equals(other.getName());
 		}
+
 		@Override
 		public int hashCode() {
 			return getName().hashCode();
@@ -33,18 +37,16 @@ public class App {
 	}
 
 	public static void main(String[] args) {
-		
-		ArrayList<Student> stuList = new ArrayList<>();
-		Student arrStu[] = {
-				new Student("张三"),
-				new Student("李四"),
-				new Student("王五"),
-				new Student("赵六")
-		};
-		stuList.addAll(Arrays.asList(arrStu));
-		
+		List<Student> stuList = Arrays.asList(new Student("张三"), new Student("李四"),new Student("王五"), new Student("赵六")  );
+
 		for (Iterator iterator = stuList.iterator(); iterator.hasNext();) {
 			Student student = (Student) iterator.next();
+			System.out.println(student.getName());
+		}
+		Student s[] = new Student[stuList.size()];
+		stuList.toArray(s);
+		
+		for (Student student : s) {
 			System.out.println(student.getName());
 		}
 		
@@ -52,11 +54,11 @@ public class App {
 		if (!stuList.contains(s)) {
 			stuList.add(s);
 		}
-
-		System.out.println("------------------");
-		for (Student student : stuList) {
-			System.out.println(student.getName());
-		}
+//
+//		System.out.println("------------------");
+//		for (Student student : stuList) {
+//			System.out.println(student.getName());
+//		}
 	}
 
 }
