a5b85409c3bf8704e0e46b203ae9b4736f8bbcb0
nbcc-java <18842426@qq.com>
2016-06-19 16:50:08 +0800

    add food image

--- src/cn/nbcc/ex29/ch09/GlassWindow.java
+++ src/cn/nbcc/ex29/ch09/GlassWindow.java
@@ -1,11 +1,21 @@
 package cn.nbcc.ex29.ch09;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 
-public class GlassWindow extends Composite implements IStatusChangeListener{
+public class GlassWindow extends Composite implements IStatusChangeListener,PaintListener{
+
+	private static final Color COLOR_DEFAULT = Display.getDefault().getSystemColor(SWT.COLOR_GRAY);
+	private static final Color COLOR_RUNNING = Display.getDefault().getSystemColor(SWT.COLOR_YELLOW);
+	private static final Image image = ImageDescriptor.createFromFile(GlassWindow.class, "asserts/pic01.jpg").createImage();
 
 	/**
 	 * Create the composite.
@@ -24,10 +34,36 @@ public class GlassWindow extends Composite implements IStatusChangeListener{
 
 	@Override
 	public void onStatusChange(MicrowaveOven oven) {
-		if (oven.getStatus()==MicrowaveOvenStatus.RUNNING) {
-			setBackground(Display.getDefault().getSystemColor(SWT.COLOR_YELLOW));
-		}else
-			setBackground(Display.getDefault().getSystemColor(SWT.COLOR_GRAY));
+		switch (oven.getStatus()) {
+		case RUNNING:
+			setBackground(COLOR_RUNNING);
+			break;
+		case FINISHED:
+			this.addPaintListener(this);
+			setBackground(COLOR_DEFAULT);
+			break;
+		default:
+			this.removePaintListener(this);
+			setBackground(COLOR_DEFAULT);
+			break;
+		}
+		redraw();
+	}
+
+	/**
+	 * 监听绘制操作
+	 */
+	@Override
+	public void paintControl(PaintEvent e) {
+		GC gc = e.gc;
+		//获取图片的宽度和高度
+		int pWidth = image.getBounds().width;
+		int pHight = image.getBounds().height;
+		//获取容器的宽度和高度
+		int cWidth = GlassWindow.this.getBounds().width;
+		int cHight = GlassWindow.this.getBounds().height;
+		//在容器中间绘制图片
+		gc.drawImage(image, (cWidth-pWidth)/2, (cHight-pHight)/2);
 	}
 
 }
